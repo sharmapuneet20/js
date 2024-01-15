e3be0b450afb860b3c95839419bdd836858e7147
sharmapuneet20 <107070137+sharmapuneet20@users.noreply.github.com>
2024-01-15 16:31:03 +0000

    datatype

---
+++ 01_basics/01_b.js
@@ -0,0 +1,9 @@
+const accountId = 232423
+let accountEmail = "pop@google.com"
+var accountPassword = "322323"
+accountCity = "chd"
+let accountState;
+// accountId =2 not allowed
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity, accountState])
